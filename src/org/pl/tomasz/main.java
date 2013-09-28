8ddf67169ebdde13a5b5d2d1f07806fdcbd0c92a
student <student@student-VirtualBox>
2013-09-28 14:59:56 +0200

    5

--- src/org/pl/tomasz/main.java
+++ src/org/pl/tomasz/main.java
@@ -3,7 +3,7 @@ package org.pl.tomasz;
 public class main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World 3 !!");
+		System.out.println("Hello World 4 !!");
 	}
 
 }
